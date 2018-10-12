ede8c103353a226f6cdb84eee0aef61ee1d50fb1
PetarPeychev <petarpeychev98@gmail.com>
2018-10-12 14:45:08 +0100

    Added eval module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ppboolean',
+      version='0.1',
+      description='Parse infix boolean expressions to RPN, evaluate and generate truth tables.',
+      url='https://github.com/PetarPeychev/ppboolean',
+      author='PetarPeychev',
+      author_email='petarpeychev98@gmail.com',
+      license='The Unlicense',
+      packages=['ppboolean'],
+      zip_safe=False)
